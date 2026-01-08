34010f8895dd79689e4dd5916060e106a1caabd3
devolexa <starovatovasasha@gmail.com>
2026-01-08 17:43:38 +0200

    update main.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import css from './App.module.css'
+import CafeInfo from '../CafeInfo/CafeInfo'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div className={css.app}>
+        <CafeInfo />
+      </div>      
     </>
   )
 }
