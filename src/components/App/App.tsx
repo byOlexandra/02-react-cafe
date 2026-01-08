0815ecf1665cd723ede9da091594c0c530da609e
devolexa <starovatovasasha@gmail.com>
2026-01-08 19:35:20 +0200

    complete App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import CafeInfo from '../CafeInfo/CafeInfo'
 import type { Votes, VoteType} from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
@@ -27,8 +28,10 @@ export default function App() {
     <>
       <div className={css.app}>
         <CafeInfo />
-        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={() => true} />
-        <VoteStats votes={votes}  totalVotes={totalVotes} positiveRates={positiveRates}/>
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0 } />
+        {totalVotes > 0
+          ? <VoteStats votes={votes} totalVotes={totalVotes} positiveRates={positiveRates} />
+          : <Notification />}        
       </div>      
     </>
   )
