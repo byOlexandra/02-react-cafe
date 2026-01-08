16e22bf9cb68f7bd5cd9ac2ec6262d6492be81dc
devolexa <starovatovasasha@gmail.com>
2026-01-08 17:32:59 +0200

    set files and normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './components/App/App.tsx'
+
+import "modern-normalize";
+import "./global.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
